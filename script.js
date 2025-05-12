33a4763423be16e2f23450bba9e4ac760f477099
Hannah Grecco <grecco327@gmail.com>
2025-05-11 23:17:03 -0300

    Metade do quiz feito

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+/*JS do Quiz botao de iniciar*/
+const click = document.getElementById("iniciar");
+const quiz = document.getElementById("quiz1");
+const next = document.getElementById("next");
+const quizn2 = document.getElementById("quiz2");
+
+click.addEventListener("click", function () {
+  click.style.display = "none";
+  next.style.display = "flex";
+  quiz.style.display = "flex";
+  quizn2.style.display = "none";
+});
+
+/*function answer(resposta) {
+  const certo = document.getElementById("right");
+  const errado1 = document.getElementById("wrong", "wrongQ2");
+  const errado2 = document.getElementById("wrong2");
+  const errado3 = document.getElementById("wrong3");
+
+  if (resposta === "right") {
+    certo.style.backgroundColor = "green";
+  } else {
+    errado1.style.backgroundColor = "red";
+    errado2.style.backgroundColor = "red";
+    errado3.style.backgroundColor = "red";
+    certo.style.backgroundColor = "green";
+  }
+}*/
+
+next.addEventListener("click", function () {
+  next.style.display = "flex";
+  quiz.style.display = "none";
+  quizn2.style.display = "flex";
+});
+/*JS do Rodapé*/
 function mostrarAgradecimento() {
   document.getElementById("formulario-inscricao").style.display = "none";
   document.getElementById("mensagem-agradecimento").style.display = "block";
